27c033d2727a48defe2e2da4ddc97fac04c10e3c
JesseLivingston1 <jesselivingston1@gmail.com>
2014-05-30 20:10:05 -0400

    about section updated. Corner images on work section.

--- js/main.js
+++ js/main.js
@@ -105,7 +105,17 @@ $(document).ready(function(){
 			$('#convergeFull').fadeOut("slow");
 			$('html').css('overflow', 'visible');
 		});
+		
+    //work full height//
 
+	$(function(){
+        var windowH = $(window).height();
+        var vintageH = $('#vintageFull, #jeremyFull, #convergeFull').height();
+        if(windowH > vintageH) {                            
+            $('#vintageFull, #jeremyFull, #convergeFull').css({'height':($(window).height())+'px'});
+    }   
+
+	});   
 
 
 //Section heights//
@@ -145,86 +155,77 @@ $(document).ready(function(){
 
 //window resize snap - not working//
 
-var STELLARJS = {
-init: function() {
-    var self = this;
-    $(function(){
-        self.$sections = $('#landing, #hero, #aboutSection, #contactSection').each(function(index){
-            $(this).data('sectionIndex', index);
-        });
+// var STELLARJS = {
+// init: function() {
+//     var self = this;
+//     $(function(){
+//         self.$sections = $('#landing, #hero, #aboutSection, #contactSection').each(function(index){
+//             $(this).data('sectionIndex', index);
+//         });
 
-        self.handleEvents();
+//         self.handleEvents();
 
-    });
-},
-handleEvents: function() {
-    var self = this,
-        //Debounce function from Underscore.js
-        debounce = function(func, wait) {
-            var timeout;
-            return function() {
-                var context = this, args = arguments;
-                var later = function() {
-                    timeout = null;
-                    func.apply(context, args);
-                };
-                clearTimeout(timeout);
-                timeout = setTimeout(later, wait);
-            }
-        },
-        handleScroll = function() {
-            var scrollTop = $(window).scrollTop(),
-                sectionIndex = Math.round((scrollTop) / self.$sections.first().outerHeight()),
-                $activeSection = self.$sections.eq(sectionIndex);
+//     });
+// },
+// handleEvents: function() {
+//     var self = this,
+//         //Debounce function from Underscore.js
+//         debounce = function(func, wait) {
+//             var timeout;
+//             return function() {
+//                 var context = this, args = arguments;
+//                 var later = function() {
+//                     timeout = null;
+//                     func.apply(context, args);
+//                 };
+//                 clearTimeout(timeout);
+//                 timeout = setTimeout(later, wait);
+//             }
+//         },
+//         handleScroll = function() {
+//             var scrollTop = $(window).scrollTop(),
+//                 sectionIndex = Math.round((scrollTop) / self.$sections.first().outerHeight()),
+//                 $activeSection = self.$sections.eq(sectionIndex);
 
-            if ($activeSection.length === 0) {
-                $activeSection = self.$sections.last();
-            }
+//             if ($activeSection.length === 0) {
+//                 $activeSection = self.$sections.last();
+//             }
 
-            if ($activeSection.length === 0) return;
+//             if ($activeSection.length === 0) return;
 
-            $(window).unbind('scroll.stellarsite');
+//             $(window).unbind('scroll.stellarsite');
 
-            if (scrollTop === 0) {
-                $(window).unbind('scroll.stellarsite').bind('scroll.stellarsite', debounce(handleScroll, 500));
-            } else {
-                $('html,body').animate({
-                    scrollTop: $activeSection.offset().top
-                }, 600, 'easeInOutExpo', function() {
-                    setTimeout(function(){
-                        $(window).unbind('scroll.stellarsite').bind('scroll.stellarsite', debounce(handleScroll, 500));
-                    }, 10);
-                });
-            }
+//             if (scrollTop === 0) {
+//                 $(window).unbind('scroll.stellarsite').bind('scroll.stellarsite', debounce(handleScroll, 500));
+//             } else {
+//                 $('html,body').animate({
+//                     scrollTop: $activeSection.offset().top
+//                 }, 600, 'easeInOutExpo', function() {
+//                     setTimeout(function(){
+//                         $(window).unbind('scroll.stellarsite').bind('scroll.stellarsite', debounce(handleScroll, 500));
+//                     }, 10);
+//                 });
+//             }
 
-            $(window).bind('mousewheel', function(){
-                $('html,body').stop(true, true);
-            });
+//             $(window).bind('mousewheel', function(){
+//                 $('html,body').stop(true, true);
+//             });
 
-            $(document).bind('keydown', function(e){
-                var key = e.which;
+//             $(document).bind('keydown', function(e){
+//                 var key = e.which;
 
-                if (key === 37 || key === 39) {
-                    $('html,body').stop(true, true);
-                }
-            });
-        };
+//                 if (key === 37 || key === 39) {
+//                     $('html,body').stop(true, true);
+//                 }
+//             });
+//         };
 
-    if (window.location.href.indexOf('#show-offset-parents-default') === -1) {
-        $(window).bind('scroll.stellarsite', debounce(handleScroll, 500));
-    }
-} }; 
+//     if (window.location.href.indexOf('#show-offset-parents-default') === -1) {
+//         $(window).bind('scroll.stellarsite', debounce(handleScroll, 500));
+//     }
+// } }; 
 
-    //work full height//
 
-	$(function(){
-        var windowH = $(window).height();
-        var vintageH = $('#vintageFull, #jeremyFull, #convergeFull').height();
-        if(windowH > vintageH) {                            
-            $('#vintageFull, #jeremyFull, #convergeFull').css({'height':($(window).height())+'px'});
-    }   
-
-	});   
 
 });
 
