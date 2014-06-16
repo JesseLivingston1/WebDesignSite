b2cc67160193e7fd50f825443f6867adc41fb358
JesseLivingston1 <jesselivingston1@gmail.com>
2014-06-16 00:35:49 -0400

    Css tweaks. Added new loading image

--- js/main.js
+++ js/main.js
@@ -31,8 +31,8 @@ if( window.addEventListener ){
 		  });
 
 		  $(closeLink).click(function() {
-            visibilityShow();
             $(container).fadeOut("medium");
+            visibilityShow();
             displayHero();
             
 		  });
